51bcfaedf84de62b829cc94f557c4af74962ffac
doardo9x <eduardo010304@gmail.com>
2024-07-29 12:20:11 -0300

    Jogo-da-Velha

---
+++ jogo-da-velha/components/figura.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { TouchableOpacity } from 'react-native'
+import { Entypo } from '@expo/vector-icons'
+
+export default function Figura() {
+  return (
+    <TouchableOpacity>
+      <Entypo name="circle" size={80} color="#000" />
+    </TouchableOpacity>
+  )
+}
